d1b3ee235e1a906b445ea39cdcc96a1d4c3b3617
Julian Denaro <juliandenaro@gmail.com>
2025-01-13 07:30:52 -0300

    renaming directories

--- 06-go-bookstore/pkg/routes/bookstore-routes.go
+++ 06-go-bookstore/pkg/routes/bookstore-routes.go
@@ -1,11 +1,15 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"go-bookstore/pkg/controllers"
+
+	"github.com/gorilla/mux"
+)
 
 var RegisteredBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookbyid).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 
